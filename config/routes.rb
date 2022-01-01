c94213dfb058362e7f0e6f4db24a5ef49e325fdf
xmaruwo <xmaruwo@gmail.com>
2022-01-01 15:48:21 +0900

    ユーザー登録、編集機能追加 投稿、編集機能追加 各アクセス制御追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  get 'login' => "users#login_form"
+  post 'login' => "users#login"
+  post 'logout' => "users#logout"
+
+  post 'users/:id/update' => "users#update"
+  get 'users/:id/edit' => "users#edit"
+  post 'users/create' => "users#create"
+  get 'signup' => "users#new"
+  get 'users/index'
+  get 'users/:id' => "users#show"
+
+  get 'posts/index' => "posts#index"
+  get 'posts/new' => "posts#new"
+  post 'posts/create' => "posts#create"
+  get 'posts/:id' => "posts#show"
+  get 'posts/:id/edit' => "posts#edit"
+  post 'posts/:id/update' => "posts#update"
+  post 'posts/:id/destroy' => "posts#destroy"
+
+  get '/' => "home#top"
+  get 'about' => "home#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
