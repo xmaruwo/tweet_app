5354c2d85c1f017894511f64b138372bacff260c
xmaruwo <xmaruwo@gmail.com>
2022-01-01 22:33:12 +0900

    いいね機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post 'likes/:post_id/create' => "likes#create"
+  post 'likes/:post_id/destroy' => "likes#destroy"
+
   get 'login' => "users#login_form"
   post 'login' => "users#login"
   post 'logout' => "users#logout"
@@ -9,6 +12,7 @@ Rails.application.routes.draw do
   get 'signup' => "users#new"
   get 'users/index'
   get 'users/:id' => "users#show"
+  get 'users/:id/likes' => "users#likes"
 
   get 'posts/index' => "posts#index"
   get 'posts/new' => "posts#new"
